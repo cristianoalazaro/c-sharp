e87a82509eb4b3024e3678f1c5362a69e43ebf93
Cristiano Ap Lazaro <cristianoalazaro@hotmail.com>
2021-11-28 10:07:34 -0300

    implementing abstract class

---
+++ DeviceAbstract/DeviceAbstract/ComboDevice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceAbstract
+{
+    internal class ComboDevice : Device, IScanner, IPrinter
+    {
+        public void Print(string document)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ProcessDoc(string documento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Scan()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DeviceAbstract/DeviceAbstract/Scanner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceAbstract
+{
+    internal class Scanner : Device, IScanner
+    {
+        public override void ProcessDoc(string documento)
+        {
+            Console.WriteLine("Scanner processing: " + documento);
+        }
+
+        public string Scan()
+        {
+            return "Scanner scan result";
+        }
+    }
+}
